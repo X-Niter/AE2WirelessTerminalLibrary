136f6ec02621599bc741066aa62d70cf481ef8a2
X_Niter <drumsforlife1999@yahoo.com>
2022-08-13 11:24:56 -0500

    Initialize A single icon for the creative tab, blank tab was odd

--- src/main/java/p455w0rd/ae2wtlib/init/LibCreativeTab.java
+++ src/main/java/p455w0rd/ae2wtlib/init/LibCreativeTab.java
@@ -1,14 +1,11 @@
 package p455w0rd.ae2wtlib.init;
 
-import java.util.List;
-import java.util.Objects;
-
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
-import p455w0rd.ae2wtlib.api.ICustomWirelessTerminalItem;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import p455w0rd.ae2wtlib.api.WTApi;
-import p455w0rd.ae2wtlib.items.ItemWUT;
 
 /**
  * @author p455w0rd
@@ -19,15 +16,9 @@ public class LibCreativeTab {
 	public static CreativeTabs CREATIVE_TAB = new CreativeTabs(WTApi.MODID) {
 
 		@Override
+		@SideOnly(Side.CLIENT)
 		public ItemStack createIcon() {
-			final List<ICustomWirelessTerminalItem> termList = WTApi.instance().getWirelessTerminalRegistry().getRegisteredTerminals(true);
-			//ensure we have more than one terminal, and account for WUT
-			final int numTerminalsRegistered = WTApi.instance().getWirelessTerminalRegistry().getNumRegisteredTerminals(true);
-			if (numTerminalsRegistered <= 1) {
-				return new ItemStack(WTApi.instance().getWirelessTerminalRegistry().getStackForHandler(termList.get(0).getClass(), false, true).getItem());
-			}
-
-			return new ItemStack(ItemWUT.getFullyStockedWut(true).getItem());
+			return new ItemStack(LibItems.ULTIMATE_TERMINAL);
 		}
 	};
 
